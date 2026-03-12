fc3b5347f7e20f7908d5f3baad2a1696f508945c
Diego Ramos <diegoramos@hotmail.com.br>
2026-03-12 12:02:25 -0300

    feat(terraform): add Karpenter NodePool with x86/arm64 and Spot/On-Demand support

---
+++ terraform/karpenter-nodepool.tf
@@ -0,0 +1,63 @@
+resource "kubectl_manifest" "karpenter_node_class" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1
+    kind: EC2NodeClass
+    metadata:
+      name: default
+    spec:
+      role: ${module.karpenter.node_iam_role_name}
+      amiSelectorTerms:
+        - alias: al2023@latest
+      subnetSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${var.cluster_name}
+      securityGroupSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${var.cluster_name}
+      tags:
+        Project: opsfleet-tech-assignment
+        ManagedBy: terraform
+        karpenter.sh/discovery: ${var.cluster_name}
+  YAML
+
+  depends_on = [helm_release.karpenter]
+}
+
+resource "kubectl_manifest" "karpenter_node_pool" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.sh/v1
+    kind: NodePool
+    metadata:
+      name: default
+    spec:
+      template:
+        spec:
+          nodeClassRef:
+            group: karpenter.k8s.aws
+            kind: EC2NodeClass
+            name: default
+          requirements:
+            - key: kubernetes.io/arch
+              operator: In
+              values: ["amd64", "arm64"]
+            - key: karpenter.sh/capacity-type
+              operator: In
+              values: ["spot", "on-demand"]
+            - key: karpenter.k8s.aws/instance-category
+              operator: In
+              values: ["c", "m", "r"]
+            - key: karpenter.k8s.aws/instance-generation
+              operator: Gt
+              values: ["5"]
+          expireAfter: 720h
+      limits:
+        cpu: "100"
+        memory: 200Gi
+      disruption:
+        consolidationPolicy: WhenEmptyOrUnderutilized
+        consolidateAfter: 30s
+      weight: 100
+  YAML
+
+  depends_on = [kubectl_manifest.karpenter_node_class]
+}
