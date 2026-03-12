c64c5b63a00d9be9eb14915f64b63fca7a8c8ee9
Diego Ramos <diegoramos@hotmail.com.br>
2026-03-12 12:01:58 -0300

    feat(terraform): add Karpenter Helm release with Pod Identity

---
+++ terraform/karpenter.tf
@@ -0,0 +1,49 @@
+module "karpenter" {
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "~> 21.0"
+
+  cluster_name = module.eks.cluster_name
+
+  # Enable Pod Identity for Karpenter (recommended over IRSA)
+  enable_pod_identity             = true
+  create_pod_identity_association = true
+
+  # Node IAM role for instances launched by Karpenter
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  tags = var.tags
+}
+
+resource "helm_release" "karpenter" {
+  namespace        = "kube-system"
+  name             = "karpenter"
+  repository       = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart            = "karpenter"
+  version          = "1.1.1"
+  wait             = false
+
+  values = [
+    <<-EOT
+    nodeSelector:
+      karpenter.sh/controller: "true"
+    tolerations:
+      - key: CriticalAddonsOnly
+        operator: Exists
+        effect: NoSchedule
+    settings:
+      clusterName: ${module.eks.cluster_name}
+      clusterEndpoint: ${module.eks.cluster_endpoint}
+      interruptionQueue: ${module.karpenter.queue_name}
+    EOT
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      repository_password
+    ]
+  }
+}
